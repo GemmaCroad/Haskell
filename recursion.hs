f3fc85a767ad15a8b85fe96e461980df06f19d66
Gemma Stiles <gemma.stiles@gmail.com>
2016-07-02 17:37:10 +1000

    Custom functions using recursion

--- recursion.hs
+++ recursion.hs
@@ -27,7 +27,39 @@ maximum' (x:xs) = max x (maximum' xs)
 --Custom version of the replicate function
 replicate' :: Int -> a -> [a]
 replicate' n x
-	| n <= 0 = []
-	| otherwise = x : replicate' (n-1) x
+    | n <= 0 = []
+    | otherwise = x : replicate' (n-1) x
 
 
+--Custom version of the take function
+take' :: (Num i, Ord i) => i -> [a] -> [a]
+take' n _
+    | n <= 0 = []
+take' _ [] = []
+take' n (x:xs) = x : take' (n-1) xs
+
+
+--Custom version of the reverse function
+reverse' :: [a] -> [a]
+reverse' [] = []
+reverse' (x:xs) = reverse' xs ++ [x]
+
+
+--Custom version of the repeat function
+repeat' :: a -> [a]
+repeat' x = x:repeat' x
+
+
+--Custom version of the zip function
+zip' :: [a] -> [b] -> [(a,b)]
+zip' _ [] = []
+zip' [] _ = []
+zip' (x:xs) (y:ys) = (x,y):zip' xs ys
+
+
+--Custom version of the elem function
+elem' :: (Eq a) => a -> [a] -> Bool
+elem' a [] = False
+elem' a (x:xs)
+    | a == x = True
+    | otherwise = a `elem'` xs
